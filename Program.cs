9e97b0f3f43ecb486866ac4f6c3aa607e4d66e26
Wvrem <131635832+Wvrem@users.noreply.github.com>
2024-12-27 23:44:12 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace تمرین_جلسه_هشتم_مبانی
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
